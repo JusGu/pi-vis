16ec605db861f5cebca623d2540385e8c1aa3a5e
jusgu <77459359+CorsaRusso@users.noreply.github.com>
2022-07-29 03:36:32 -0400

    created color and mockup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import GraphPI from './components/graphComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GraphPI></GraphPI>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/graphComponent.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function GraphPI(props){
+    return(
+        <div>Test</div>
+    );
+}
+
+export default GraphPI;
